95f825747a14a2c7b3a2e63ae8a8da736668ae51
AlliedProduct <nojusnoah@gmail.com>
2023-11-21 21:26:07 +0000

    Add files via upload
    Contributors - Nojus Sinkevicius & Domantas Kynzelis

---
+++ WHOProject/src/main/java/WHO/WHOProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package WHO;
+
+/**
+ *
+ * @author nojus
+ */
+public class WHOProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
